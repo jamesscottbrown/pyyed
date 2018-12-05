6b977e9e0e1087dae1daae3363ac934d611745e8
Johan Lundstroem <lundstrom.se@gmail.com>
2018-12-05 12:52:49 +0100

    Added support to output graph to file on storage
    Added a write_to_file function to enable easy file output of the demo graphs in demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,15 @@
 from __future__ import print_function
 import pyyed
 
+def write_to_file(graph, file_index):
+    file_name = "test_graph_%s.graphml" % str(file_index)
+    inp = input("\nWould you like to write this data to file: %s? (y/n): " % file_name)
+    if "y" in inp.lower():
+        file_handle = open(file_name, "w")
+        file_handle.write(g.get_graph())
+        file_handle.close()
+        print("Graph written to %s" % file_name)
+
 g = pyyed.Graph()
 
 g.add_node('foo', font_family="Zapfino")
@@ -19,6 +28,7 @@ g.add_edge('foo', 'foo1', label="EDGE!", width="3.0", color="#0000FF",
            arrowhead="white_diamond", arrowfoot="standard", line_type="dotted")
 
 print(g.get_graph())
+write_to_file(g, 1)
 
 print("\n\n\n")
 
@@ -34,6 +44,7 @@ g.add_edge('foo2', 'abc')
 g.add_edge('foo', 'MY_Group')
 
 print(g.get_graph())
+write_to_file(g, 2)
 
 
 print("\n\n\n")
@@ -58,3 +69,4 @@ g.add_edge('Car', 'ICar', arrowhead="white_delta", line_type="dashed")
 g.add_node('This is a note', shape_fill="#EEEEEE", node_type="UMLNoteNode")
 
 print(g.get_graph())
+write_to_file(g, 3)
