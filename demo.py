b78679a6ca74d2ef0fc3cc8fb5c0bf3da0feed42
Johan Lundstroem <lundstrom.se@gmail.com>
2019-01-24 17:55:53 +0100

    Changed to use write_graph() instad of write(g.get_graph())

--- demo.py
+++ demo.py
@@ -1,13 +1,11 @@
 from __future__ import print_function
 import pyyed
 
-def write_to_file(graph, file_index):
+def write_to_file(graph, file_index=0):
     file_name = "test_graph_%s.graphml" % str(file_index)
     inp = input("\nWould you like to write this data to file: %s? (y/n): " % file_name)
     if "y" in inp.lower():
-        file_handle = open(file_name, "w")
-        file_handle.write(g.get_graph())
-        file_handle.close()
+        graph.write_graph(file_name)
         print("Graph written to %s" % file_name)
 
 g = pyyed.Graph()
@@ -28,7 +26,7 @@ g.add_edge('foo', 'foo1', label="EDGE!", width="3.0", color="#0000FF",
            arrowhead="white_diamond", arrowfoot="standard", line_type="dotted")
 
 print(g.get_graph())
-write_to_file(g, 1)
+write_to_file(g, file_index=1)
 
 print("\n\n\n")
 
@@ -44,7 +42,7 @@ g.add_edge('foo2', 'abc')
 g.add_edge('foo', 'MY_Group')
 
 print(g.get_graph())
-write_to_file(g, 2)
+write_to_file(g, file_index=2)
 
 
 print("\n\n\n")
@@ -69,4 +67,4 @@ g.add_edge('Car', 'ICar', arrowhead="white_delta", line_type="dashed")
 g.add_node('This is a note', shape_fill="#EEEEEE", node_type="UMLNoteNode")
 
 print(g.get_graph())
-write_to_file(g, 3)
+write_to_file(g, file_index=3)
