21764681ff556b9c5aac9370f30f610fe7bec517
James Scott-Brown <james@jamesscottbrown.com>
2020-12-21 11:38:33 +0000

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.command.install import install as _install
 
 
 setup(name='pyyed',
-      version='1.4.2',
+      version='1.4.3',
       description='A simple Python library to export graphs to the yEd graph editor',
 
       author='James Scott-Brown',
